aaf04d694c9ddca00c3e329f85188b0a3db2c26a
fjuras <fjuras@student.42wolfsburg.de>
2022-11-28 17:47:18 +0100

    Added check_variant function

--- test-parser/test.c
+++ test-parser/test.c
@@ -6,7 +6,7 @@
 /*   By: fjuras <fjuras@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 20:12:03 by fjuras            #+#    #+#             */
-/*   Updated: 2022/11/27 21:48:44 by fjuras           ###   ########.fr       */
+/*   Updated: 2022/11/28 17:46:39 by fjuras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,220 +19,218 @@
 
 t_env	g_env;
 
-int	test_single_cmd(const char *filter)
+#define check_variant(...) check_variant_x(__FILE__, __LINE__, __VA_ARGS__)
+int	check_variant_x(const char *from, int line, t_env env, t_line exp, char *input)
 {
-	t_line	line;
-	t_line	expect;
+	t_line	parsed_line;
+	int		r;
+
+	parsed_line = parse(input, env);
+	r = test_expect_line_eq_x(from, line, &parsed_line, &exp);
+	test_line_free(parsed_line);
+	return (r);
+}
+
+int	test_framework_leak(const char *filter)
+{
+	t_line	exp;
 	int		i;
-	int		res;
 
 	TEST_START(filter);
 	i = 0;
-	test_line_init(&expect, 1);
-	test_prog_args(&expect.progs[i], "ls", NULL);
-	test_prog_redirs(&expect.progs[i++], NULL, NULL);
-	test_line_end(&expect, i);
-	line = parse("ls", g_env);
-	res = test_expect_line_eq(&line, &expect);
-	test_line_free(line); test_line_free(expect);
-	return (TEST_END(res));
+	test_line_init(&exp, 2);
+	test_prog_args(&exp.progs[i], "ls", "-l", NULL);
+	test_prog_redirs(&exp.progs[i++], "in.txt", NULL);
+	test_prog_args(&exp.progs[i], "grep", "total", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, "out.txt");
+	test_line_end(&exp, i);
+	test_line_free(exp);
+	return (TEST_END(1));
+}
+
+int	test_single_cmd(const char *filter)
+{
+	t_line	exp;
+	int		i, r;
+
+	TEST_START(filter);
+	i = 0;
+	test_line_init(&exp, 1);
+	test_prog_args(&exp.progs[i], "ls", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, NULL);
+	test_line_end(&exp, i);
+	r = check_variant(g_env, exp, "ls") &&
+		check_variant(g_env, exp, " ls ");
+	test_line_free(exp);
+	return (TEST_END(r));
 }
 
 int	test_cmd_with_arg(const char *filter)
 {
-	t_line	line;
-	t_line	expect;
-	int		i;
-	int		res;
+	t_line	exp;
+	int		i, r;
 
 	TEST_START(filter);
 	i = 0;
-	test_line_init(&expect, 1);
-	test_prog_args(&expect.progs[i], "grep", "total", NULL);
-	test_prog_redirs(&expect.progs[i++], NULL, NULL);
-	test_line_end(&expect, i);
-	line = parse("grep total", g_env);
-	res = test_expect_line_eq(&line, &expect);
-	test_line_free(line); test_line_free(expect);
-	return (TEST_END(res));
+	test_line_init(&exp, 1);
+	test_prog_args(&exp.progs[i], "grep", "total", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, NULL);
+	test_line_end(&exp, i);
+	r = check_variant(g_env, exp, "grep total");
+	test_line_free(exp);
+	return (TEST_END(r));
 }
 
 int	test_two_cmds(const char *filter)
 {
-	t_line	line;
-	t_line	expect;
-	int		i;
-	int		res;
+	t_line	exp;
+	int		i, r;
 
 	TEST_START(filter);
 	i = 0;
-	test_line_init(&expect, 2);
-	test_prog_args(&expect.progs[i], "ls", "-l", NULL);
-	test_prog_redirs(&expect.progs[i++], NULL, NULL);
-	test_prog_args(&expect.progs[i], "grep", "total", NULL);
-	test_prog_redirs(&expect.progs[i++], NULL, NULL);
-	test_line_end(&expect, i);
-	line = parse("ls -l | grep total", g_env);
-	res = test_expect_line_eq(&line, &expect);
-	test_line_free(line); test_line_free(expect);
-	return (TEST_END(res));
+	test_line_init(&exp, 2);
+	test_prog_args(&exp.progs[i], "ls", "-l", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, NULL);
+	test_prog_args(&exp.progs[i], "grep", "total", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, NULL);
+	test_line_end(&exp, i);
+	r = check_variant(g_env, exp, "ls -l | grep total");
+	test_line_free(exp);
+	return (TEST_END(r));
 }
 
 int	test_redir(const char *filter)
 {
-	t_line	line;
-	t_line	expect;
-	int		i;
-	int		res;
+	t_line	exp;
+	int		i, r;
 
 	TEST_START(filter);
 	i = 0;
-	test_line_init(&expect, 1);
-	test_prog_args(&expect.progs[i], "ls", NULL);
-	test_prog_redirs(&expect.progs[i++], "in.txt", "out.txt");
-	test_line_end(&expect, i);
-	line = parse("<in.txt ls >out.txt", g_env);
-	res = test_expect_line_eq(&line, &expect);
-	test_line_free(line); test_line_free(expect);
-	return (TEST_END(res));
+	test_line_init(&exp, 1);
+	test_prog_args(&exp.progs[i], "ls", NULL);
+	test_prog_redirs(&exp.progs[i++], "in.txt", "out.txt");
+	test_line_end(&exp, i);
+	r = check_variant(g_env, exp, "<in.txt ls >out.txt");
+	test_line_free(exp);
+	return (TEST_END(r));
 }
 
 int	test_env(const char *filter)
 {
-	t_line	line;
-	t_line	expect;
-	int		i;
-	int		res;
+	t_line	exp;
+	int		i, r;
 
 	TEST_START(filter);
 	i = 0;
-	test_line_init(&expect, 1);
-	test_prog_args(&expect.progs[i], "a", NULL);
-	test_prog_redirs(&expect.progs[i++], NULL, NULL);
-	test_line_end(&expect, i);
-	line = parse("$A", g_env);
-	res = test_expect_line_eq(&line, &expect);
-	test_line_free(line); test_line_free(expect);
-	return (TEST_END(res));
+	test_line_init(&exp, 1);
+	test_prog_args(&exp.progs[i], "a", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, NULL);
+	test_line_end(&exp, i);
+	r = check_variant(g_env, exp, "$A");
+	test_line_free(exp);
+	return (TEST_END(r));
 }
 
 int	test_double_quote(const char *filter)
 {
-	t_line	line;
-	t_line	expect;
-	int		i;
-	int		res;
+	t_line	exp;
+	int		i, r;
 
 	TEST_START(filter);
 	i = 0;
-	test_line_init(&expect, 1);
-	test_prog_args(&expect.progs[i], "one string", NULL);
-	test_prog_redirs(&expect.progs[i++], NULL, NULL);
-	test_line_end(&expect, i);
-	line = parse("\"one string\"", g_env);
-	res = test_expect_line_eq(&line, &expect);
-	test_line_free(line); test_line_free(expect);
-	return (TEST_END(res));
+	test_line_init(&exp, 1);
+	test_prog_args(&exp.progs[i], "one string", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, NULL);
+	test_line_end(&exp, i);
+	r = check_variant(g_env, exp, "\"one string\"");
+	test_line_free(exp);
+	return (TEST_END(r));
 }
 
 
 int	test_single_quote(const char *filter)
 {
-	t_line	line;
-	t_line	expect;
-	int		i;
-	int		res;
+	t_line	exp;
+	int		i, r;
 
 	TEST_START(filter);
 	i = 0;
-	test_line_init(&expect, 1);
-	test_prog_args(&expect.progs[i], "one string", NULL);
-	test_prog_redirs(&expect.progs[i++], NULL, NULL);
-	test_line_end(&expect, i);
-	line = parse("\'one string\'", g_env);
-	res = test_expect_line_eq(&line, &expect);
-	test_line_free(line); test_line_free(expect);
-	return (TEST_END(res));
+	test_line_init(&exp, 1);
+	test_prog_args(&exp.progs[i], "one string", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, NULL);
+	test_line_end(&exp, i);
+	r = check_variant(g_env, exp, "\'one string\'");
+	test_line_free(exp);
+	return (TEST_END(r));
 }
 
 int	test_dq_env(const char *filter)
 {
-	t_line	line;
-	t_line	expect;
-	int		i;
-	int		res;
+	t_line	exp;
+	int		i, r;
 
 	TEST_START(filter);
 	i = 0;
-	test_line_init(&expect, 1);
-	test_prog_args(&expect.progs[i], "a", NULL);
-	test_prog_redirs(&expect.progs[i++], NULL, NULL);
-	test_line_end(&expect, i);
-	line = parse("\"$A\"", g_env);
-	res = test_expect_line_eq(&line, &expect);
-	test_line_free(line); test_line_free(expect);
-	return (TEST_END(res));
+	test_line_init(&exp, 1);
+	test_prog_args(&exp.progs[i], "a", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, NULL);
+	test_line_end(&exp, i);
+	r = check_variant(g_env, exp, "\"$A\"");
+	test_line_free(exp);
+	return (TEST_END(r));
 }
 
 int	test_env_w_space(const char *filter)
 {
-	t_line	line;
-	t_line	expect;
-	int		i;
-	int		res;
+	t_line	exp;
+	int		i, r;
 
 	TEST_START(filter);
 	i = 0;
-	test_line_init(&expect, 1);
-	test_prog_args(&expect.progs[i], "ls", "-l", NULL);
-	test_prog_redirs(&expect.progs[i++], NULL, NULL);
-	test_line_end(&expect, i);
-	line = parse("$LS", g_env);
-	res = test_expect_line_eq(&line, &expect);
-	test_line_free(line); test_line_free(expect);
-	return (TEST_END(res));
+	test_line_init(&exp, 1);
+	test_prog_args(&exp.progs[i], "ls", "-l", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, NULL);
+	test_line_end(&exp, i);
+	r = check_variant(g_env, exp, "$LS");
+	test_line_free(exp);
+	return (TEST_END(r));
 }
 
 int	test_dq_env_w_space(const char *filter)
 {
-	t_line	line;
-	t_line	expect;
-	int		i;
-	int		res;
+	t_line	exp;
+	int		i, r;
 
 	TEST_START(filter);
 	i = 0;
-	test_line_init(&expect, 1);
-	test_prog_args(&expect.progs[i], "ls -l", NULL);
-	test_prog_redirs(&expect.progs[i++], NULL, NULL);
-	test_line_end(&expect, i);
-	line = parse("\"$LS\"", g_env);
-	res = test_expect_line_eq(&line, &expect);
-	test_line_free(line); test_line_free(expect);
-	return (TEST_END(res));
+	test_line_init(&exp, 1);
+	test_prog_args(&exp.progs[i], "ls -l", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, NULL);
+	test_line_end(&exp, i);
+	r = check_variant(g_env, exp, "\"$LS\"");
+	test_line_free(exp);
+	return (TEST_END(r));
 }
 
 int	test_sq_env(const char *filter)
 {
-	t_line	line;
-	t_line	expect;
-	int		i;
-	int		res;
+	t_line	exp;
+	int		i, r;
 
 	TEST_START(filter);
 	i = 0;
-	test_line_init(&expect, 1);
-	test_prog_args(&expect.progs[i], "$A", NULL);
-	test_prog_redirs(&expect.progs[i++], NULL, NULL);
-	test_line_end(&expect, i);
-	line = parse("\'$A\'", g_env);
-	res = test_expect_line_eq(&line, &expect);
-	test_line_free(line); test_line_free(expect);
-	return (TEST_END(res));
+	test_line_init(&exp, 1);
+	test_prog_args(&exp.progs[i], "$A", NULL);
+	test_prog_redirs(&exp.progs[i++], NULL, NULL);
+	test_line_end(&exp, i);
+	r = check_variant(g_env, exp, "\'$A\'");
+	test_line_free(exp);
+	return (TEST_END(r));
 }
 
 const t_test_function g_test_functions[] =
 {
+	test_framework_leak,
 	test_single_cmd,
 	test_cmd_with_arg,
     test_two_cmds,
