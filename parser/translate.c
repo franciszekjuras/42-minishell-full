99ab88494540fbdb6ee3d59153e362e9d8c9adee
chpark <chpags01@gmail.com>
2022-12-18 17:04:44 +0100

    fix_norminette_v02

--- parser/translate.c
+++ parser/translate.c
@@ -12,7 +12,7 @@
 
 #include "parser.h"
 
-int	ft_cmd_len(t_cmd *cur_cmd)
+static int	ft_cmd_len(t_cmd *cur_cmd)
 {
 	int		len;
 	t_cmd	*tmp_cmd;
@@ -52,32 +52,27 @@ t_line	translate(t_cmd *cmd)
 			{
 				line.progs[idx].in_redir.path = ft_strdup(cur_cmd->argv[++i]);
 				line.progs[idx].in_redir.is_alt = 0;
-				// printf("path: %s\n", line.progs[idx].in_redir.path);
 			}
 			else if (ft_strcmp(cur_cmd->argv[i], ">") == 0)
 			{
 				line.progs[idx].out_redir.path = ft_strdup(cur_cmd->argv[++i]);
 				line.progs[idx].out_redir.is_alt = 0;
-				// printf("path: %s\n", line.progs[idx].out_redir.path);
 			}
 			else if (ft_strcmp(cur_cmd->argv[i], "<<") == 0)
 			{
 				line.progs[idx].in_redir.path = ft_strdup(cur_cmd->argv[++i]);
 				line.progs[idx].in_redir.is_alt = 1;
-				// printf("path: %s\n", line.progs[idx].in_redir.path);
 			}
 			else if (ft_strcmp(cur_cmd->argv[i], ">>") == 0)
 			{
 				line.progs[idx].out_redir.path = ft_strdup(cur_cmd->argv[++i]);
 				line.progs[idx].out_redir.is_alt = 1;
-				// printf("path: %s\n", line.progs[idx].out_redir.path);
 			}
 			else
 			{
 				line.progs[idx].args[j++] = ft_strdup(cur_cmd->argv[i]);
 			}
 			i++;
-			// printf("line: %d %s\n", j, line.progs[idx].args[j]);
 		}
 		idx++;
 		cur_cmd = cur_cmd->next;
